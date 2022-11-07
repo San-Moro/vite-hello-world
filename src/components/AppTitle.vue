0bec7377a48deca265c9272353433fdf4615ff71
San-Moro <valentinacortese.dsg@gmail.com>
2022-11-07 16:06:27 +0100

    ticket - error in console

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+import AppTitle from "./components/AppTitle.vue";
+
+export default {
+    data(){
+        return {
+    },
+    components: {
+        AppTitle
+    },
+    methods: {}
+}
+</script>
+
+<template>
+    <AppTitle />
+</template>
+
+<style></style>
